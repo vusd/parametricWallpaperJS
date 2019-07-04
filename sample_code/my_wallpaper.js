9daa992fdcf3c09b79872e3c344ec280e3f80f85
Tom White <tom.white@vuw.ac.nz>
2019-07-05 07:54:24 +1200

    Updated p5.js version and cleanup up sample code

--- sample_code/my_wallpaper.js
+++ sample_code/my_wallpaper.js
@@ -2,22 +2,21 @@
 var thingy_width  = 20;
 var thingy_height = 20;
 
-function setup_wallpaper(pWallpaper){
+function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true);//set this to false when you're ready to print
+  pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 100;
   pWallpaper.grid_settings.cell_height = 100;
   pWallpaper.grid_settings.row_offset  = 50;
-
 }
 
-function wallpaper_background(){
-  background(255,255,240);
+function wallpaper_background() {
+  background(255, 255, 240);
 }
 
-function my_symbol(x, y){
-  rect(x,y,thingy_width, thingy_height);
+function my_symbol(x, y) {
+  rect(x ,y, thingy_width, thingy_height);
 }
